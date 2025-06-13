53a7418dbd5590bba74b3afaaa19863f4fb7034a
bhavishy09 <futuresingh111@gmail.com>
2025-06-13 18:58:43 +0530

    middleware and error handling in express.js

---
+++ middleware.js
@@ -0,0 +1,39 @@
+const express=require("express")
+
+const app = express();
+
+//server p kiyni bhi request ajye phele y chlega 
+app.use(function(req,res,next){
+
+    console.log("middleware chl gya ");
+    next();
+
+})
+
+app.get("/", function(req, res)  {
+  res.send("i m katariya ")
+})
+app.get("/about", function(req, res)  {
+    res.send("about page ayega ")
+  })
+  app.get("/profile", function(req, res)  {
+    res.send("profile page ayega ")
+  })
+
+
+//error handling 
+
+//this goes on terminal
+app.get("/profile", function(req, res,next)  {
+    return next(new error("something worng  "))
+  });
+
+
+
+//this goes on frontend 
+  app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
+  })
+
+app.listen(3004);
--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 
-import express from 'express'
+const express=require("express")
 
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
+app.get("/", function(req, res)  {
+  res.send("i m katariya ")
 })
 
-app.listen(3000)
+app.get("/profile", function(req, res)  {
+    res.send("i m best ")
+  })
+  
+
+app.listen(3003);
