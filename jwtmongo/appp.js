68739dc9e4d03a84fa88e4606b07c2b3665d5c2b
bhavishy09 <futuresingh111@gmail.com>
2025-07-14 00:36:25 +0530

    full login logout and registor pages via jwt auth and bcrypt

--- jwtmongo/appp.js
+++ jwtmongo/appp.js
@@ -3,6 +3,7 @@ const cookieparser = require('cookie-parser');
 const bcrypt = require('bcrypt');
 const path = require('path');
 const usermodel = require("./models/user");
+const jwt=require('jsonwebtoken')
 
 const app = express();
 app.use(cookieparser());
@@ -27,10 +28,43 @@ app.post('/create', (req, res) => {
         password: hash,
         age
       });
-
+       const token=jwt.sign({email},"shhhhhh");
+    
+       res.cookie("token",token)
       res.send(createdUser);
     });
   });
 });
+
+app.get("/logout",(req,res)=>{
+    res.cookie("token","")
+    res.redirect("/")
+
+})
+
+
+
+
+app.post("/login",async (req,res)=>{
+
+    let user= await usermodel.findOne({email:req.body.email});
+    if(!user)  return res.send("something worng");
+
+bcrypt.compare(req.body.password, user.password, function (err, result) {
+    if (result) {
+        let token = jwt.sign({ email: user.email }, "shhhhhhhhhhh");
+        res.cookie("token", token);
+        res.send("yes you can login");
+    }
+    else res.send("something is wrong");
+})
+      
+});
+
+app.get("/login",(req,res)=>{
+    res.render("login")
+})
+
+
 
 app.listen(3001);
